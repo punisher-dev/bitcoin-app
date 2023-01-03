6a99d1f829e14828e223fd8c1bba0635add3a5cf
Guscosta88 <guslfcosta@gmail.com>
2023-01-03 14:50:40 +1100

    Cors

--- src/BitcoinIndex.jsx
+++ src/BitcoinIndex.jsx
@@ -11,7 +11,7 @@ const BitcoinIndex = ({ currency="AUD" }) => {
     
     // Mount and currency change
     useEffect(() => {
-        fetch(`https://justcors.com/tl_71374ab/http://api.coindesk.com/v1/bpi/currentprice/${currency}.json`)
+        fetch(`https://justcors.com/tl_df2ac41/http://api.coindesk.com/v1/bpi/currentprice/${currency}.json`)
             .then((res) => res.json())
             .then((data) => setPrice(data.bpi[currency].rate_float))
     }, [currency])
