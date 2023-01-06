86113b7b487f5677ea8e64c10ad473f41a054a88
Guscosta88 <guslfcosta@gmail.com>
2023-01-06 13:54:20 +1100

    cors

--- src/CurrencySelector.jsx
+++ src/CurrencySelector.jsx
@@ -5,7 +5,7 @@ const CurrencySelector = ({ currency, setCurrency }) => {
 
     
     useEffect(() => {
-        fetch("https://justcors.com/tl_df2ac41/http://api.coindesk.com/v1/bpi/supported-currencies.json")
+        fetch("https://justcors.com/tl_676e6b9/http://api.coindesk.com/v1/bpi/supported-currencies.json")
             .then((res) => res.json())
             .then((data) => setCurrencies(data))
     }, [])
